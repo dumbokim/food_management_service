3731fc7f6e63dff271ba25950346657a2d91d609
dumbokim <rlawlsgus97@gmail.com>
2022-11-22 04:03:13 +0900

    리뷰 등록 기능 수정

--- lib/pages/main/review/register_review.dart
+++ lib/pages/main/review/register_review.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_ppopgi/common/common.dart';
 import 'package:food_ppopgi/data/food/food_data_repository.dart';
 import 'package:food_ppopgi/domain/domain.dart';
+import 'package:food_ppopgi/pages/main/main.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'register_review_state.dart';
@@ -24,7 +26,7 @@ class RegisterReviewPage extends ConsumerWidget {
 
     final state = ref.watch(notifier);
 
-    WidgetsBinding.instance?.addPostFrameCallback((_) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       if (state is RegisterReviewStateRegistered) {
         Navigator.pop(context, state);
       }
@@ -33,6 +35,7 @@ class RegisterReviewPage extends ConsumerWidget {
     final read = ref.read(notifier.notifier);
 
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: Column(
           children: [
@@ -45,128 +48,147 @@ class RegisterReviewPage extends ConsumerWidget {
         ),
         centerTitle: true,
       ),
+      floatingActionButton: RegisterButton(
+        text: '등록하기',
+        onTap: () {
+          read.register(
+            context,
+            title: _titleController.text,
+            content: _contentController.text,
+          );
+        },
+      ),
       body: Stack(
         children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-            child: Column(
-              children: [
-                Row(
-                  children: const [
-                    Text(
-                      '리뷰 남기기',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  decoration: BoxDecoration(
-                      border: Border.all(width: 2, color: Colors.grey),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: TextField(
+          GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: Container(
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              child: Column(
+                children: [
+                  Row(
+                    children: const [
+                      Text(
+                        '리뷰 남기기',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
                     controller: _titleController,
                     style: TextStyle(fontSize: 20),
                     decoration: const InputDecoration(
-                      focusedBorder: InputBorder.none,
-                      border: InputBorder.none,
+                      border: OutlineInputBorder(),
+                      hintText: '제목',
+                      counterText: '',
                     ),
+                    maxLines: 1,
+                    maxLength: 15,
                   ),
-                ),
-                const SizedBox(height: 10),
-                GestureDetector(
-                  onTap: () => _contentFocus.requestFocus(),
-                  child: Container(
-                    height: 200,
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    decoration: BoxDecoration(
-                        border: Border.all(width: 2, color: Colors.grey),
-                        borderRadius: BorderRadius.circular(10)),
+                  const SizedBox(height: 15),
+                  GestureDetector(
+                    onTap: () => _contentFocus.requestFocus(),
                     child: TextField(
                       controller: _contentController,
                       focusNode: _contentFocus,
                       style: TextStyle(fontSize: 20),
                       decoration: const InputDecoration(
-                        focusedBorder: InputBorder.none,
-                        border: InputBorder.none,
+                        border: OutlineInputBorder(),
+                        hintText: '내용',
                       ),
+                      maxLines: 6,
+                      maxLength: 120,
                     ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                GestureDetector(
-                  onTap: () {
-                    showModalBottomSheet(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                          child: Column(
-                            children: [
-                              const Text('점수 선택'),
-                              const SizedBox(height: 20),
-                              Expanded(
-                                child: CupertinoPicker(
-                                  scrollController: FixedExtentScrollController(
-                                    initialItem: state.score,
+                  const SizedBox(height: 20),
+                  GestureDetector(
+                      onTap: () {
+                        showModalBottomSheet(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 10),
+                              child: Column(
+                                children: [
+                                  const Text(
+                                    '점수 선택',
+                                    style: TextStyle(fontSize: 20),
                                   ),
-                                  diameterRatio: 2,
-                                  itemExtent: 60,
-                                  onSelectedItemChanged: (int index) {
-                                    read.selectScore(index);
-                                  },
-                                  children: List.generate(
-                                      101, (index) => Text('$index')),
-                                ),
+                                  const SizedBox(height: 20),
+                                  Expanded(
+                                    child: CupertinoPicker(
+                                      scrollController:
+                                          FixedExtentScrollController(
+                                        initialItem: state.score,
+                                      ),
+                                      itemExtent: 60,
+                                      magnification: 1.5,
+                                      onSelectedItemChanged: (int index) {
+                                        read.selectScore(index);
+                                      },
+                                      children: List.generate(
+                                        6,
+                                        (index) => Center(
+                                          child: Text(
+                                            index == 0
+                                                ? '0'
+                                                : List.generate(
+                                                        index, (index) => '🍚')
+                                                    .join(),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text(
+                                      '확인',
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                  )
+                                ],
                               ),
-                              const SizedBox(height: 10),
-                              TextButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: const Text('확인'),
-                              )
-                            ],
-                          ),
+                            );
+                          },
                         );
                       },
-                    );
-                  },
-                  child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey.withOpacity(0.2),
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 10),
-                      alignment: Alignment.center,
-                      width: 100,
-                      child: Text(
-                        '점수 : ${state.score}',
-                        style: TextStyle(fontSize: 20),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(
+                                color: defaultColor,
+                                width: 2,
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 10),
+                            alignment: Alignment.center,
+                            child: Text(
+                              '점수 : ${List.generate(state.score, (index) => '🍚').join()}',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: defaultColor,
+                              ),
+                            ),
+                          ),
+                        ],
                       )),
-                ),
-                const SizedBox(height: 10),
-                TextButton(
-                  onPressed: () {
-                    read.register(
-                      context,
-                      title: _titleController.text,
-                      content: _contentController.text,
-                    );
-                  },
-                  child: const Text(
-                    '등록하기',
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           if (state is RegisterReviewStateLoading)
@@ -185,7 +207,7 @@ class RegisterReviewPage extends ConsumerWidget {
 
 class RegisterReviewStateNotifier extends StateNotifier<RegisterReviewState> {
   RegisterReviewStateNotifier(this.repository)
-      : super(RegisterReviewStateInitial(0));
+      : super(RegisterReviewStateInitial(5));
 
   final FoodRepository repository;
 
